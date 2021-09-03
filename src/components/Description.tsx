49ccb06351677e7f23739edac1b2b99b94b7b768
kanako-110 <kanako.110.ito@gmail.com>
2021-09-03 19:43:22 +0900

    feat: checkbox

--- src/components/Description.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export const Description = () => {
-  return <div>Lets find your MOTE-country together</div>
-}
--- src/components/FirstView.tsx
+++ src/components/FirstView.tsx
@@ -1,7 +1,10 @@
 // TODO: 命名
 
 import { Button } from './atmos/Button';
-import { Description } from './Description';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import { Checkbox, FormControlLabel } from '@material-ui/core';
 
 interface Props {
 	onClick: () => void;
@@ -10,8 +13,30 @@ interface Props {
 export const FirstView = ({ onClick }: Props) => {
 	return (
 		<div>
-      {/* 湧けなくてもいいかも */}
-			<Description /> 
+			<div>Lets find your MOTE-country together</div>
+			<p>
+				モテない。。。。。。。。。そんなあなたに！！！あなたがモテモテになる国を見つけましょう！
+			</p>
+			{/* TODO えらー */}
+			<div>
+				<FormControl component="fieldset">
+					<FormLabel component="legend">一番近い体格を選んでください</FormLabel>
+					<FormGroup>
+						<FormControlLabel control={<Checkbox />} label="細め" />
+						<FormControlLabel control={<Checkbox />} label="ぽっちゃり" />
+						<FormControlLabel control={<Checkbox />} label="グラマラス" />
+					</FormGroup>
+				</FormControl>
+
+				<FormControl component="fieldset">
+					<FormLabel component="legend">一番近い性格を選んでください</FormLabel>
+					<FormGroup>
+						<FormControlLabel control={<Checkbox />} label="知的" />
+						<FormControlLabel control={<Checkbox />} label="陽気" />
+						<FormControlLabel control={<Checkbox />} label="家庭的" />
+					</FormGroup>
+				</FormControl>
+			</div>
 			<Button onClick={onClick} />
 		</div>
 	);
