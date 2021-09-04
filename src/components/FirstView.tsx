b8f007dc9af45d66e817625f69cf607849ecc5e7
kanako-110 <kanako.110.ito@gmail.com>
2021-09-04 22:03:14 +0900

    chore: handleChange

--- src/components/FirstView.tsx
+++ src/components/FirstView.tsx
@@ -4,34 +4,73 @@ import { Button } from './atmos/Button';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
-import { Checkbox, FormControlLabel } from '@material-ui/core';
+import { Checkbox, FormControlLabel, FormHelperText } from '@material-ui/core';
+import { useState } from 'react';
 
 interface Props {
 	onClick: () => void;
 }
 
 export const FirstView = ({ onClick }: Props) => {
+	const choices: string[] = [];
+	const [firstChoice, setFirstChoice] = useState({
+		slender: false,
+		chubby: false,
+		glamorous: false,
+	});
+
+	const { slender, chubby, glamorous } = firstChoice;
+	const error = [slender, chubby, glamorous].filter((v) => v).length !== 1;
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setFirstChoice({
+			...firstChoice,
+			[event.target.name]: event.target.checked,
+		});
+
+		// 2つの選択肢を合わせた配列を作る
+		choices.splice(1, 0, event.target.name);
+		console.log(choices);
+	};
 	return (
 		<div>
 			<div>Lets find your MOTE-country together</div>
 			<p>
 				モテない。。。。。。。。。そんなあなたに！！！あなたがモテモテになる国を見つけましょう！
 			</p>
-			{/* TODO えらー */}
+			{/* TODO 2個以上、2個以下 えらー */}
 			<div>
-				<FormControl component="fieldset">
-					<FormLabel component="legend">一番近い体格を選んでください</FormLabel>
+				<FormControl required error={error} component="fieldset">
+					<FormLabel component="legend">１番近い体格を選んでください</FormLabel>
 					<FormGroup>
-						<FormControlLabel control={<Checkbox />} label="細め" />
-						<FormControlLabel control={<Checkbox />} label="ぽっちゃり" />
-						<FormControlLabel control={<Checkbox />} label="グラマラス" />
+						{/* ハートもできる */}
+						<FormControlLabel
+							control={<Checkbox onChange={handleChange} />}
+							label="細め"
+							name="slender"
+						/>
+						<FormControlLabel
+							control={<Checkbox />}
+							label="ぽっちゃり"
+							name="chubby"
+						/>
+						<FormControlLabel
+							control={<Checkbox />}
+							label="グラマラス"
+							name="glamorous"
+						/>
 					</FormGroup>
+					{/* TODO:選択してから表示させる */}
+					{error && <FormHelperText>必ず１つ選択してください</FormHelperText>}
 				</FormControl>
-
 				<FormControl component="fieldset">
-					<FormLabel component="legend">一番近い性格を選んでください</FormLabel>
+					<FormLabel component="legend">１番近い性格を選んでください</FormLabel>
 					<FormGroup>
-						<FormControlLabel control={<Checkbox />} label="知的" />
+						<FormControlLabel
+							control={<Checkbox onChange={handleChange} />}
+							label="知的"
+							name="smart"
+						/>
 						<FormControlLabel control={<Checkbox />} label="陽気" />
 						<FormControlLabel control={<Checkbox />} label="家庭的" />
 					</FormGroup>
